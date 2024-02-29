0288d73ec73c6c18251f436bcad836122237bbc6
amora7741 <mora7741@gmail.com>
2024-02-28 17:47:49 -0800

    Change font, setup starting card

---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+// function Button({ text }) {
+//   return <button onClick={}>{text}</button>;
+// }
+
+// export default Button;
--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,7 +1,18 @@
+// import Button from './Button';
+
 function StartScreen({ setgameStatus, setDifficulty }) {
   return (
     <div className='card'>
-      <h1>Play Game</h1>
+      <div className='info'>
+        <h1>Pokémon: Battle Recon Zone</h1>
+        <p>Try not to select the same card twice.</p>
+        <p>
+          The higher the difficulty, the more cards there are to choose from.
+        </p>
+        <p>See how far you can go!</p>
+      </div>
+      <p>Select your difficulty:</p>
+      <div className='buttons'>{/* <Button text={'Easy'}></Button> */}</div>
     </div>
   );
 }
