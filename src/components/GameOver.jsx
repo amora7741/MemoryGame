e6426a5d592f852d0a7629574068932203843ada
amora7741 <mora7741@gmail.com>
2024-03-01 21:50:38 -0800

    Setup game over screen

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import PikaDub from '../assets/pikachudub.gif';
+import PikaL from '../assets/pikachul.gif';
+
 function GameOver({ playerWon, handleRetry }) {
+  const picsrc = playerWon ? PikaDub : PikaL;
+  const result = playerWon ? 'You Won! :3' : 'You Lost :(';
+
   return (
-    <>
-      <h1>Game Over</h1>
-      <p>Player Won: {playerWon.toString()}</p>
-      <button onClick={handleRetry}>Retry</button>
-    </>
+    <motion.main
+      className='endscreen'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+    >
+      <h1>{result}</h1>
+      <img src={picsrc} alt='' />
+    </motion.main>
   );
 }
 
